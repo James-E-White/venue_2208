41f4e60cda52b2f10b88729a00f3de1fdf4cc60f
James White <whitejaad@gmail.com>
2022-08-24 09:04:43 -0700

    Add patron update

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,4 +1,32 @@
 class Venue
+  attr_reader :name,
+              :capacity,
+              :patrons,
+              :yell_at_patrons
+
   def initialize(name, capacity)
-    
+    @name = name
+    @capacity = capacity
+    @patrons = []
+    @yell_at_patrons = []
+  end
+
+   def add_patron(patron)
+     @patrons << patron
+   end
+
+   def yell_at_patrons
+     patrons.each do |patron|
+       yell_at_patrons << patron.upcase
+     end
+ end
 end
+#   def capacity?
+#     capacity > 4
+#       true
+#     else < 4
+#       false
+#
+# end
+# puts capacity
+# puts name
--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -1,5 +1,5 @@
 require 'rspec'
-require './lib/venue'
+require './lib/venue.rb'
 
 describe Venue do
   describe '#initialize' do
@@ -9,19 +9,18 @@ describe Venue do
     end
 
     it 'can read the name' do
-      skip
       venue = Venue.new('Bluebird', 4)
       expect(venue.name).to eq 'Bluebird'
     end
 
     it 'can read the capacity' do
-      skip
+
       venue = Venue.new('Bluebird', 4)
       expect(venue.capacity).to eq 4
     end
 
     it 'has no patrons by default' do
-      skip
+
       venue = Venue.new('Bluebird', 4)
       expect(venue.patrons).to eq []
     end
@@ -31,7 +30,7 @@ describe Venue do
 
   describe '#add_patron' do
     it 'returns a list of patrons' do
-      skip
+
       venue = Venue.new('Bluebird', 4)
       venue.add_patron('Mike')
       venue.add_patron('Megan')
